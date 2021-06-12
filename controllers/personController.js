f088f302c9059a538056c23f2c8a7eb4c53f5642
Joaquin Zamora <ezamora@ec.krugercorp.com>
2021-06-11 22:51:20 -0500

    person update

--- controllers/personController.js
+++ controllers/personController.js
@@ -21,3 +21,24 @@ exports.findPersons = async (req, res) => {
         res.status(500).send('An error has occurred');
     }
 }
+
+exports.updatePerson = async (req, res) => {
+    try {
+        const { firstname, lastname, birthday } = req.body;
+        let person = await Person.findById(req.params.id);
+
+        if (!person) {
+            res.status(400).send({msg: 'Person not found'});
+        }
+
+        person.firstname = firstname;
+        person.lastname = lastname;
+        person.birthday = birthday;
+
+        person = await Person.findOneAndUpdate({_id: req.params.id}, person, {new: true});
+        res.send(person);
+    } catch (error) {
+        console.log(error);   
+        res.status(500).send('An error has occurred');
+    }
+}
--- routes/person.js
+++ routes/person.js
@@ -4,5 +4,6 @@ const personController = require('../controllers/personController');
 
 router.post('/', personController.createPerson);
 router.get('/', personController.findPersons);
+router.put('/:id', personController.updatePerson);
 
 module.exports = router;
